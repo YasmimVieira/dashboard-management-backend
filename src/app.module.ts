063b43fabddb080e2386a880c8902d2fb018b7f8
YasmimVieira <vieiray937@gmail.com>
2026-05-24 15:33:31 -0300

    fix: reorganize files and structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database/database.module';
+import { AuthModule } from './auth/auth/auth.module';
+import { AuthService } from './auth/services/auth/auth.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
