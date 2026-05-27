af37f5b810d82daeeb0dd92218775ba9c0ece816
YasmimVieira <vieiray937@gmail.com>
2026-05-26 21:20:42 -0300

    feat: refactor authentication and user modules; add items module with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database/database.module';
 import { AuthModule } from './auth/auth/auth.module';
+import { ItemsService } from './items/items/items.service';
+import { ItemsController } from './items/items/items.controller';
+import { Controller } from './items/.controller';
+import { ItemsController } from './items/controller/items/items.controller';
+import { ItemsService } from './items/service/items/items.service';
+import { ServiceService } from './items/service/service.service';
+import { ItemsService } from './items/service/items/items.service';
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -12,7 +19,7 @@ import { AuthModule } from './auth/auth/auth.module';
 
 @Module({
   imports: [AuthModule, DatabaseModule, UsersModule, ItemsModule],
-  controllers: [AppController],
-  providers: [AppService, AuthService],
+  controllers: [AppController, ItemsController, Controller],
+  providers: [AppService, AuthService, ItemsService, ServiceService],
 })
 export class AppModule {}
