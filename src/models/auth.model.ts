364502de976488db45c1fe426c64a7dae6011fa8
YasmimVieira <vieiray937@gmail.com>
2026-05-30 14:08:37 -0300

    fix: add class validator

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class Auth {
+    @IsEmail()
     email!: string;
+
+    @IsString()
+    @MinLength(6)
     password!: string;
+
+    @IsOptional()
+    @IsString()
     name?: string;
 }
