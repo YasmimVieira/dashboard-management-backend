b86f8f0faf2bafee18c786ad49d60bdb5012ca6c
YasmimVieira <vieiray937@gmail.com>
2026-05-29 22:58:25 -0300

    fix: changed envs

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,11 +5,11 @@ export const typeOrmConfig = registerAs(
   'database',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT ?? '5432'),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    synchronize: Boolean(process.env.DB_SYNC ?? false),
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT ?? '5432'),
+    username: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    synchronize: Boolean(process.env.DATABASE_SYNC ?? false),
     ssl: {
       rejectUnauthorized: false,
     },
