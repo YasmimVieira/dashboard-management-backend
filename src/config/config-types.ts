4a714b2d80af315f2ccc91289deaa6ddcb743719
YasmimVieira <vieiray937@gmail.com>
2026-05-29 23:00:21 -0300

    fix: changed envs

--- src/config/config-types.ts
+++ src/config/config-types.ts
@@ -9,10 +9,10 @@ export interface ConfigType {
 
 export const appConfigSchema = Joi.object({
   APP_MESSAGE_PREFIX: Joi.string().default('Hello '),
-  DB_HOST: Joi.string().default('localhost'),
-  DB_PORT: Joi.number().default(5432),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
+  DATABASE_HOST: Joi.string().default('localhost'),
+  DATABASE_PORT: Joi.number().default(5432),
+  DATABASE_NAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
   // DB_DATABASE: Joi.string().required(),
-  DB_SYNC: Joi.number().valid(0, 1).required(),
+  DATABASE_SYNC: Joi.number().valid(0, 1).required(),
 });
